377304d669bc047bf1007cb185b14632af7b0f2f
ASUS <anastasia.pronina@inbox.ru>
2022-05-12 00:31:14 +0300

    changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let formOpener = document.querySelector('.profile__edit');
-let content = document.querySelector('.popup');
+const formOpener = document.querySelector('.profile__edit');
+const content = document.querySelector('.popup');
 function popupOpened() {
     content.classList.add('popup__opened');
 }
 formOpener.addEventListener('click', popupOpened);
 
-let formCloser = document.querySelector('.popup__close-button');
+const formCloser = document.querySelector('.popup__close-button');
 function popupClosed() {
     content.classList.remove('popup__opened');
 }
 formCloser.addEventListener('click', popupClosed);
 
-let formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__name');
 let jobInput = formElement.querySelector('.popup__comment');
 let profileAuthor = document.querySelector('.profile__author');
